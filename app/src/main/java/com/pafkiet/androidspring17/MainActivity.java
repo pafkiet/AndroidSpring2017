e9f581d036b1768c40ba35bacfec834b01503ded
Khawar <khawarhere@gmail.com>
2017-01-26 00:46:26 +0500

    Lecture-02, User Interface, and layouts

--- app/src/main/java/com/pafkiet/androidspring17/MainActivity.java
+++ app/src/main/java/com/pafkiet/androidspring17/MainActivity.java
@@ -2,12 +2,18 @@ package com.pafkiet.androidspring17;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Button mButtonAbout = (Button) findViewById(R.id.main_button_about);
+        mButtonAbout.setText("PAF Button");
     }
 }
